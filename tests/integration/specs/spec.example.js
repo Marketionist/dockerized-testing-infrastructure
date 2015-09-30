93e10fa76280e2e5c283940bd0497d5ce8bd7c5b
Marketionist <marketionist@gmail.com>
2015-09-30 18:04:49 +0300

    Added basic integration tests infrastructure

---
+++ tests/integration/specs/spec.example.js
@@ -0,0 +1,22 @@
+'use strict';
+/* global describe, it, browser, expect */
+
+// #############################################################################
+// INTEGRATION TEST
+var examplePage = require('../pages/page.example.js');
+var cmsProtractorHelper = require('cms-protractor-helper');
+
+describe('Example integration tests: ', function () {
+
+    it('opens the site and validates the logo', function () {
+        // go to the main page
+        browser.get(examplePage.site);
+
+        // wait for site logo to appear
+        cmsProtractorHelper.waitFor(examplePage.githubLogo);
+
+        // validate logo is displayed
+        expect(examplePage.githubLogo.isDisplayed()).toBeTruthy();
+    });
+
+});
